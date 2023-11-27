988938ebcaa66899ea9efad58cbc9c4320bf4b94
predapaula4 <predapaula@yahoo.com>
2023-11-27 13:14:44 +0200

    am poza cu tabla in grid

--- LaddersAndSnakesFinal/src/main/java/com/example/laddersandsnakesfinal/Controller/BoardController.java
+++ LaddersAndSnakesFinal/src/main/java/com/example/laddersandsnakesfinal/Controller/BoardController.java
@@ -5,6 +5,7 @@ import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.image.Image ;
+import javafx.scene.layout.Region;
 
 public class BoardController {
     @FXML
@@ -17,11 +18,26 @@ public class BoardController {
     public void initialize() {
         // Set the background image
         Image backgroundImage = new Image(getClass().getResource("/l2.jpg").toExternalForm());
-        ImageView backgroundImageView = new ImageView(backgroundImage);
-        backgroundImageView.setFitWidth(500);
-        backgroundImageView.setFitHeight(500);
-        gridpane.getChildren().add(backgroundImageView);
 
+        gridpane.setStyle("-fx-background-image: url('" + backgroundImage.getUrl() + "'); " +
+                "-fx-background-size: 525px 525px; " +
+                "-fx-background-repeat: no-repeat; " +
+                "-fx-background-position: center center;");
+
+        int numCells = 10;
+        double cellSize = 50.0;
+
+        for (int row = 0; row < numCells; row++) {
+            for (int col = 0; col < numCells; col++) {
+                Region cell = new Region();
+                cell.setMinSize(cellSize, cellSize);
+                cell.setMaxSize(cellSize, cellSize);
+                cell.setStyle("-fx-border-color: black; -fx-padding: 10px;");
+                gridpane.add(cell, col, row);
+            }
+        }
+
+        gridpane.toBack();
     }
     @FXML
     protected void onExitButtonClick() {
