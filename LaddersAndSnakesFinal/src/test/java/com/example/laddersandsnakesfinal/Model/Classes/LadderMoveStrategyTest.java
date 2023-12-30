47969600465c6c8a170a6a382eae100c70fba4ce
predapaula4 <predapaula@yahoo.com>
2023-12-30 12:44:42 +0200

    mockito

--- LaddersAndSnakesFinal/src/test/java/com/example/laddersandsnakesfinal/Model/Classes/LadderMoveStrategyTest.java
+++ LaddersAndSnakesFinal/src/test/java/com/example/laddersandsnakesfinal/Model/Classes/LadderMoveStrategyTest.java
@@ -2,76 +2,62 @@ package com.example.laddersandsnakesfinal.Model.Classes;
 
 import com.example.laddersandsnakesfinal.Model.Interfaces.IMoveStrategy;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
-import java.util.Arrays;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 class LadderMoveStrategyTest {
     @Test
     public void testHandleMove1() {
-        List<Integer> laddersPos = Arrays.asList(5, 9);
-        IMoveStrategy ladderMoveStrategy = new LadderMoveStrategy(laddersPos);
-        int initialPosition = 5;
-        int newPosition = ladderMoveStrategy.handleMove(initialPosition);
-        assertEquals(9, newPosition);
+        testHandleMove(5, 9);
     }
+
     @Test
     public void testHandleMove2() {
-        List<Integer> laddersPos = Arrays.asList(15, 25);
-        IMoveStrategy ladderMoveStrategy = new LadderMoveStrategy(laddersPos);
-        int initialPosition = 15;
-        int newPosition = ladderMoveStrategy.handleMove(initialPosition);
-        assertEquals(25, newPosition);
+        testHandleMove(15, 25);
     }
+
     @Test
     public void testHandleMove3() {
-        List<Integer> laddersPos = Arrays.asList(18, 80);
-        IMoveStrategy ladderMoveStrategy = new LadderMoveStrategy(laddersPos);
-        int initialPosition = 18;
-        int newPosition = ladderMoveStrategy.handleMove(initialPosition);
-        assertEquals(80, newPosition);
+        testHandleMove(18, 80);
     }
+
     @Test
     public void testHandleMove4() {
-        List<Integer> laddersPos = Arrays.asList(44, 86);
-        IMoveStrategy ladderMoveStrategy = new LadderMoveStrategy(laddersPos);
-        int initialPosition = 44;
-        int newPosition = ladderMoveStrategy.handleMove(initialPosition);
-        assertEquals(86, newPosition);
+        testHandleMove(44, 86);
     }
+
     @Test
     public void testHandleMove5() {
-        List<Integer> laddersPos = Arrays.asList(47, 68);
-        IMoveStrategy ladderMoveStrategy = new LadderMoveStrategy(laddersPos);
-        int initialPosition = 47;
-        int newPosition = ladderMoveStrategy.handleMove(initialPosition);
-        assertEquals(68, newPosition);
+        testHandleMove(47, 68);
     }
 
     @Test
     public void testHandleMove6() {
-        List<Integer> laddersPos = Arrays.asList(63, 78);
-        IMoveStrategy ladderMoveStrategy = new LadderMoveStrategy(laddersPos);
-        int initialPosition = 63;
-        int newPosition = ladderMoveStrategy.handleMove(initialPosition);
-        assertEquals(78, newPosition);
+        testHandleMove(63, 78);
     }
+
     @Test
     public void testHandleMove7() {
-        List<Integer> laddersPos = Arrays.asList(71, 94);
-        IMoveStrategy ladderMoveStrategy = new LadderMoveStrategy(laddersPos);
-        int initialPosition = 71;
-        int newPosition = ladderMoveStrategy.handleMove(initialPosition);
-        assertEquals(94, newPosition);
+        testHandleMove(71, 94);
     }
+
     @Test
     public void testHandleMove8() {
-        List<Integer> laddersPos = Arrays.asList(81, 98);
-        IMoveStrategy ladderMoveStrategy = new LadderMoveStrategy(laddersPos);
-        int initialPosition = 81;
-        int newPosition = ladderMoveStrategy.handleMove(initialPosition);
-        assertEquals(98, newPosition);
+        testHandleMove(81, 98);
+    }
+
+    private void testHandleMove(int initialPosition, int expectedNewPosition) {
+        // Creăm un mock pentru IMoveStrategy
+        IMoveStrategy mockMoveStrategy = Mockito.mock(IMoveStrategy.class);
+
+        // Definim comportamentul mock-ului
+        when(mockMoveStrategy.handleMove(initialPosition)).thenReturn(expectedNewPosition);
+
+        // Apelăm metoda și verificăm rezultatul
+        int actualNewPosition = mockMoveStrategy.handleMove(initialPosition);
+        assertEquals(expectedNewPosition, actualNewPosition);
     }
 }
