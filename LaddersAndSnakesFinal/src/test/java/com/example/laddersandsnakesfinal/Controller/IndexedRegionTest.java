8533e81392f6468fdf08da023f1826c8bf4085ab
predapaula4 <predapaula@yahoo.com>
2024-01-03 10:06:58 +0200

    decumentatie teste
    Co-Authored-By: PopaRazvan97 <116148998+PopaRazvan97@users.noreply.github.com>

--- LaddersAndSnakesFinal/src/test/java/com/example/laddersandsnakesfinal/Controller/IndexedRegionTest.java
+++ LaddersAndSnakesFinal/src/test/java/com/example/laddersandsnakesfinal/Controller/IndexedRegionTest.java
@@ -10,19 +10,15 @@ class IndexedRegionTest {
     @Test
     public void testInitialization() {
         IndexedRegion indexedRegion = new IndexedRegion(1);
-
         // Verificăm că obiectul a fost creat cu succes
         assertNotNull(indexedRegion);
-
         // Verificăm că indexul a fost setat corect
         assertEquals(1, indexedRegion.getIndex());
-
         // Verificăm că dimensiunile sunt setate corect
         assertEquals(50.0, indexedRegion.getMinWidth());
         assertEquals(50.0, indexedRegion.getMinHeight());
         assertEquals(50.0, indexedRegion.getMaxWidth());
         assertEquals(50.0, indexedRegion.getMaxHeight());
-
         // Verificăm că stilul a fost setat corect
         assertEquals("-fx-border-color: black; -fx-padding: 10px;", indexedRegion.getStyle());
     }
@@ -37,9 +33,5 @@ class IndexedRegionTest {
         // Verificăm că indexul a fost actualizat
         assertEquals(2, indexedRegion.getIndex());
     }
-
-
-
-
 
 }
